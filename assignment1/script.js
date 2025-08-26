955736410dc4819db581ed43a1f55eb0c7b80261
Jonhson031 <jonhson228124@gmail.com>
2025-08-25 22:00:01 -0500

    assignment1

---
+++ assignment1/script.js
@@ -0,0 +1,5 @@
+let name = " Maksym!";
+document.getElementById('name').innerText = document.getElementById('name').innerText + name;
+console.log("my name is" + name);
+
+// My program has a variable with my name and I use this variable to change my text in HTML
