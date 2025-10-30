4ba547291ca54f3f5310c7f195536e1ebf60c689
Jonhson031 <jonhson228124@gmail.com>
2025-10-30 13:16:48 -0500

    update

--- session10/scripts/activity2.js
+++ session10/scripts/activity2.js
@@ -1,5 +1,6 @@
 'use strict';
 const books = [];
+
 document.getElementById("book-btn").addEventListener("click", function () {
     const title = document.getElementById("book-title").value.trim();
     const author = document.getElementById("book-author").value.trim();
@@ -7,20 +8,13 @@ document.getElementById("book-btn").addEventListener("click", function () {
     const publisher = document.getElementById("book-publisher").value.trim();
     const city = document.getElementById("book-city").value.trim();
     const state = document.getElementById("book-state").value.trim();
+    const format = document.getElementById("format").value;
     if (!title || !author || !year || !publisher || !city || !state) {
         alert("Please fill in all fields!");
         return;
     }
-
-    const book = {
-        title: title,
-        author: author,
-        year: year,
-        publisher: publisher,
-        city: city,
-        state: state
-    };
-
+   
+    const book = { title, author, year, publisher, city, state, format };
     books.push(book);
     books.sort((a, b) => a.author.localeCompare(b.author));
     displaySorted();
@@ -29,29 +23,19 @@ document.getElementById("book-btn").addEventListener("click", function () {
 });
 function displaySorted() {
     const list = document.querySelector(".book-list");
-    const selectedFormat = document.getElementById('select-format').value.toLowerCase();
     list.innerHTML = "";
 
     books.forEach(book => {
         let formatted = "";
 
-        if (selectedFormat === "apa") {
+        if (book.format === "APA") {
             formatted = `${book.author} (${book.year}). <i>${book.title}</i>. ${book.city}, ${book.state}: ${book.publisher}.`;
-            const li = document.createElement("li");
-            li.innerHTML = `${formatted} <br>`;
-            list.appendChild(li);
-        } else if (selectedFormat === 'mla') {
+        } else {
             formatted = `${book.author}. <i>${book.title}</i>. ${book.city}, ${book.state}: ${book.publisher}, ${book.year}.`;
-            const li = document.createElement("li");
-            li.innerHTML = `${formatted} <br>`;
-            list.appendChild(li);
-        } else{
-            alert('Choose format');
         }
 
-
-        // const li = document.createElement("li");
-        // li.innerHTML = `${formatted} <br>`;
-        // list.appendChild(li);
+        const li = document.createElement("li");
+        li.innerHTML = `${formatted} <br><small>(${book.format} format)</small>`;
+        list.appendChild(li);
     });
 }
